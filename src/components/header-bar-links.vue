2241ce42707b38851f191a7138e0d211c416b987
edunogodz <edunogodz@gmail.com>
2020-04-18 13:45:32 -0300

    04-lists-and-conditionals-end

--- src/components/header-bar-links.vue
+++ src/components/header-bar-links.vue
@@ -3,18 +3,10 @@
     <div class="navbar-end">
       <div class="navbar-item">
         <div class="buttons">
-          <a
-            :href="github"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a :href="github" target="_blank" rel="noopener noreferrer">
             <i class="fab fa-github fa-2x" aria-hidden="true"></i>
           </a>
-          <a
-            :href="twitter"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a :href="twitter" target="_blank" rel="noopener noreferrer">
             <i class="fab fa-twitter fa-2x" aria-hidden="true"></i>
           </a>
         </div>
@@ -25,11 +17,10 @@
 
 <script>
 export default {
-  name: "HeaderBarLinks",
   data() {
     return {
       github: 'https://github.com/johnpapa/vue-getting-started',
-      twitter: 'https://twitter.com/john_papa'
+      twitter: 'https://twitter.com/john_papa',
     };
   },
 };
