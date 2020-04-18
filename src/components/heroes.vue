17ad064615d9ca7c7b0246fa2914a1fcfe0e625d
edunogodz <edunogodz@gmail.com>
2020-04-18 14:09:00 -0300

    06-component-communication-end

--- src/components/heroes.vue
+++ src/components/heroes.vue
@@ -2,109 +2,44 @@
   <div class="content-container">
     <div class="section content-title-group">
       <h2 class="title">Heroes</h2>
-    </div>
-    <div class="columns">
-      <div class="column is-3">
-        <div class="card" v-show="heroes.length">
-          <header class="card-header">
-            <p class="card-header-title">heroes list</p>
-          </header>
-          <ul class="list is-hoverable">
+      <div class="columns">
+        <div class="column is-8" v-if="heroes">
+          <!-- <heroes-list> -->
+          <ul v-if="!selectedHero">
             <li v-for="hero in heroes" :key="hero.id">
-              <a
-                class="list-item"
-                @click="selectHero(hero)"
-                :class="{ 'is-active': selectedHero === hero }"
-              >
-                <span>{{ hero.firstName }}</span>
-              </a>
+              <div class="card">
+                <div class="card-content">
+                  <div class="content">
+                    <div :key="hero.name" class="name">
+                      {{ hero.firstName }} {{ hero.lastName }}
+                    </div>
+                    <div class="description">{{ hero.description }}</div>
+                  </div>
+                </div>
+                <footer class="card-footer">
+                  <button
+                    class="link card-footer-item"
+                    @click="selectHero(hero)"
+                  >
+                    <i class="fas fa-check"></i>
+                    <span>Select</span>
+                  </button>
+                </footer>
+              </div>
             </li>
           </ul>
-        </div>
-        <div class="notification is-info" v-show="message">{{ message }}</div>
-      </div>
+          <!-- <heroes-list> -->
 
-      <div class="column is-4" v-if="selectedHero">
-        <div class="card">
-          <header class="card-header">
-            <!-- <p class="card-header-title">{{ selectedHero.firstName }}</p> -->
-            <p class="card-header-title">{{ fullName }}</p>
-          </header>
-          <div class="card-content">
-            <div class="content">
-              <div class="field">
-                <label class="label" for="id">id</label>
-                <label class="input" id="id" readonly>{{
-                  selectedHero.id
-                }}</label>
-              </div>
-              <div class="field">
-                <label class="label" for="firstName">first name</label>
-                <input
-                  class="input"
-                  id="firstName"
-                  v-model="selectedHero.firstName"
-                />
-              </div>
-              <div class="field">
-                <label class="label" for="lastName">last name</label>
-                <input
-                  class="input"
-                  id="lastName"
-                  v-model="selectedHero.lastName"
-                />
-              </div>
-              <div class="field">
-                <label class="label" for="description">description</label>
-                <input
-                  class="input"
-                  id="description"
-                  v-model="selectedHero.description"
-                />
-              </div>
-              <div class="field">
-                <label class="label" for="originDate">description</label>
-                <input
-                  type="date"
-                  class="input"
-                  id="originDate"
-                  v-model="selectedHero.originDate"
-                />
-                <p class="comment">
-                  My origin story began on
-                  {{ selectedHero.originDate | shortDate }}
-                </p>
-              </div>
-              <div class="field">
-                <label class="label" for="capeCounter">cape counter</label>
-                <input
-                  class="input"
-                  id="capeCounter"
-                  type="number"
-                  v-model="selectedHero.capeCounter"
-                />
-              </div>
-              <div class="field">
-                <label class="label" for="capeMessage">cape message</label>
-                <label class="input" name="capeMessage">{{
-                  capeMessage
-                }}</label>
-              </div>
-            </div>
-          </div>
-          <footer class="card-footer">
-            <button
-              class="link card-footer-item cancel-button"
-              @click="cancelHero()"
-            >
-              <i class="fas fa-undo"></i>
-              <span>Cancel</span>
-            </button>
-            <button class="link card-footer-item" @click="saveHero()">
-              <i class="fas fa-save"></i>
-              <span>Save</span>
-            </button>
-          </footer>
+          <!-- <hero-detail> -->
+          <HeroDetail 
+            v-if="selectedHero" 
+            :hero="selectedHero"
+            @cancel="cancelHero"
+            @save="saveHero"
+          />
+          <!-- </hero-detail> -->
+
+          <div class="notification is-info" v-show="message">{{ message }}</div>
         </div>
       </div>
     </div>
@@ -112,61 +47,26 @@
 </template>
 
 <script>
-import { format } from 'date-fns';
-const inputDateFormat = 'YYYY-MM-DD';
-const displayDateFormat = 'MMM DD, YYYY';
-const ourHeroes = [
-  {
-    id: 10,
-    firstName: 'Ella',
-    lastName: 'Papa',
-    capeCounter: 1,
-    originDate: format(new Date(1996, 0, 1), inputDateFormat),
-    description: 'fashionista',
-  },
-  {
-    id: 20,
-    firstName: 'Madelyn',
-    lastName: 'Papa',
-    capeCounter: 3,
-    originDate: format(new Date(1999, 4, 1), inputDateFormat),
-    description: 'the cat whisperer',
-  },
-  {
-    id: 30,
-    firstName: 'Haley',
-    lastName: 'Papa',
-    capeCounter: 2,
-    originDate: format(new Date(1991, 8, 1), inputDateFormat),
-    description: 'pen wielder',
-  },
-  {
-    id: 40,
-    firstName: 'Landon',
-    lastName: 'Papa',
-    capeCounter: 0,
-    originDate: format(new Date(2000, 9, 1), inputDateFormat),
-    description: 'arc trooper',
-  },
-];
+import { ourHeroes, lifecycleHooks, heroWatchers, logger } from '../shared';
+
+import HeroDetail from '@/components/hero-detail';
+
 export default {
   name: 'Heroes',
   data() {
     return {
-      heroes: ourHeroes,
+      heroes: [],
       selectedHero: undefined,
       message: '',
       capeMessage: '',
     };
   },
-  computed: {
-    fullName() {
-      return `${this.selectedHero.firstName} ${this.selectedHero.lastName}`;
-    },
-  },
   created() {
     this.loadHeroes();
+    logger.info(`${this.componentName} created hook called`);
   },
+  components: { HeroDetail },  
+  mixins: [lifecycleHooks, heroWatchers],
   methods: {
     async getHeroes() {
       return new Promise(resolve => {
@@ -179,49 +79,18 @@ export default {
       this.heroes = await this.getHeroes();
       this.message = '';
     },
-    handleTheCapes(newValue) {
-      const value = parseInt(newValue, 10);
-      switch (value) {
-        case 0:
-          this.capeMessage = 'Where is my cape?';
-          break;
-        case 1:
-          this.capeMessage = 'One is all I need';
-          break;
-        case 2:
-          this.capeMessage = 'Alway have a spare';
-          break;
-        default:
-          this.capeMessage = 'You can never have enough capes';
-          break;
-      }
-    },
     cancelHero() {
       this.selectedHero = undefined;
-      this.message = '';
     },
-    saveHero() {
-      // This only updates when you click the save button
-      this.message = JSON.stringify(this.selectedHero, null, '\n ');
+    saveHero(hero) {
+      const index = this.heroes.findIndex(h => h.id === hero.id);
+      this.heroes.splice(index, 1, hero);
+      this.heroes = [...this.heroes];
+      this.selectedHero = undefined;
     },
     selectHero(hero) {
       this.selectedHero = hero;
-    },
-  },
-  watch: {
-    'selectedHero.capeCounter': {
-      immediate: true,
-      handler(newValue, oldValue) {
-        console.log(`Watcher evaluated. old=${oldValue}, new=${newValue}`);
-        // console.log(`Watcher evalauted. old=${oldValue}, new=${newValue}`);
-        this.handleTheCapes(newValue);
-      },
-    },
-  },
-  filters: {
-    shortDate: function(value) {
-      return format(value, displayDateFormat);
-    },
+    }, 
   },
 };
 </script>
